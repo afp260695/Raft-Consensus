28b4fcb09b6c4e51dd194744430c29682b7b17bc
Ahmad Fajar Prasetiyo <ahmad.fajar260695@gmail.com>
2017-04-27 21:59:24 +0700

    fixing raft

--- node.js
+++ node.js
@@ -13,42 +13,44 @@ var data = []
 var nodeData = [
 		{
 			ip: '127.0.0.1',
-			proxy: '3001' 
+			port: '3001' 
 		},
 		{
 			ip: '127.0.0.1',
-			proxy: '3002' 
+			port: '3002' 
 		},
 		{
 			ip: '127.0.0.1',
-			proxy: '3003' 
+			port: '3003' 
 		},
 		{
 			ip: '127.0.0.1',
-			proxy: '3004' 
+			port: '3004' 
 		},
 		{
 			ip: '127.0.0.1',
-			proxy: '3005' 
+			port: '3005' 
 		},
 	];
 
 var selfData = parseInt(process.argv[2]) // Example to run: node node.js 0
-var leader = 0
+var leader = {}
 var setVote = new Set()
 var timeInterval = getRandomInt((10*1000), (10*1300))
 
 var inter = reInterval(function () {
 	if(state == 1){
-		timeInterval = getRandomInt((1000), (1300))
-		console.log("Send election")
+		timeInterval = getRandomInt((10*1000), (10*1300))
+		console.log('Vote size: '+setVote.size)
 		if(setVote.size >= 3) {
+			console.log('Succes become leader')
 			sendHeartbreath()
 			timeInterval = 200
 			state = 2
 		} else {
+			console.log('Back to follower state')
 			setVote = new Set()
-			broadcastElection()
+			state = 0
 		}
 
 		inter.reschedule(timeInterval) // reset schedule
@@ -56,7 +58,7 @@ var inter = reInterval(function () {
 		state = 1
 		console.log("Send election")
 		broadcastElection()
-		timeInterval = 200
+		timeInterval = 5000
 		inter.reschedule(timeInterval)
 	} else {
 		console.log("Send hearthbreath")
@@ -67,36 +69,57 @@ var inter = reInterval(function () {
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-app.post('/', urlencodedParser, function (req, res) { //respon election
-	if(state == 0) {
-		leader.ip = req.body.ip
-		leader.proxy = req.body.proxy
+app.post('/', urlencodedParser, function (req, res) { //respon Heartbreath
+	console.log("Follower")
+	leader.ip = req.body.ip
+	leader.port = req.body.port
+	inter.reschedule(timeInterval)
+	res.send('1')
+})
+
+app.get('/', function (req, res) { // respon election
+	
+	if(state == 0) {	
 		inter.reschedule(timeInterval)
-		res.send(String(nodeData[selfData].proxy))
+		res.send(String(nodeData[selfData].port))
 	} else {
 		res.send('0')
 	}
-})
-
-app.get('/', function (req, res) { // respon Heartbreath
-	console.log("Follower")
-	inter.reschedule(timeInterval)
-	res.send('1')
 })
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-function broadcastElection() {
+function sendHeartbreath() {
 	_.forEach(nodeData, function(obj,i){
 		if(i != selfData){
-			let url = 'http://'+obj.ip+':'+obj.proxy+'/' 
+			let url = 'http://'+obj.ip+':'+obj.port+'/' 
 			console.log('Send to :'+url)
 			axios({
 				method: 'post',
 				url: url,
 				data: nodeData[selfData]
+			})
+			.then((data) => {
+				console.log('Succes: '+data.data)
+				
+			})
+			.catch((err) => {
+				console.log('Error: '+err)
+			})
+		}
+	})
+}
+
+function broadcastElection() {
+	_.forEach(nodeData, function(obj,i){
+		if(i != selfData){
+			let url = 'http://'+obj.ip+':'+obj.port+'/' 
+			console.log('Send to :'+url)
+			axios({
+				method: 'get',
+				url: url
 			})
 			.then((data) => {
 				console.log('Succes: '+data.data)
@@ -110,24 +133,5 @@ function broadcastElection() {
 		}
 	})
 }
-
-function sendHeartbreath() {
-	_.forEach(nodeData, function(obj,i){
-		if(i != selfData){
-			let url = 'http://'+obj.ip+':'+obj.proxy+'/' 
-			console.log('Send to :'+url)
-			axios({
-				method: 'get',
-				url: url
-			})
-			.then((data) => {
-				console.log('Succes: '+data.data)
-			})
-			.catch((err) => {
-				console.log('Error: '+err)
-			})
-		}
-	})
-}
  
-app.listen(nodeData[selfData].proxy)
+app.listen(nodeData[selfData].port)
