29d092a17d75808d0bcdd3af9e293ad9e78eb5d4
Robert Sebastian Herlim <ngiooong@yahoo.com>
2017-04-28 18:05:28 +0700

    Rapihkan kode part 2

--- app.js
+++ app.js
@@ -31,6 +31,9 @@ const LEADER_STATE = 2
 
 const MAJORITY = 3 // Jumlah node total = 5
 
+const ELECTION_TIMEOUT = 3000
+const HEARTBEAT_TIMEOUT = 2000
+
 var state = FOLLOWER_STATE
 var selfTerm = 0
 var nodeData = [
@@ -74,45 +77,147 @@ var serverData = [
 	]
 
 var selfData = parseInt(process.argv[2]) // Example to run: node node.js 0
-var leader = {}
+var leader = {}	
 var setVote = new Set()
 var timeInterval = getRandomInt((10*700), (10*900))
+
+/***********************************************
+ *             RAFT FUNCTIONS
+ *
+ ***********************************************/
+
+/*
+ *  Generate random integer within interval
+ *
+ */
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
 
 /*
  *  Timeout function for each node.
  *
  */
 var inter = reInterval(function () {
-	if(state == CANDIDATE_STATE){
+	console.log('==============================================');
+	if (state == FOLLOWER_STATE) {
+		state = CANDIDATE_STATE
+
+		console.log("Send election to all nodes")
+		selfTerm++
+		setVote = new Set()
+		setVote.add(nodeData[selfData])
+
+		broadcastElection()
+
+		timeInterval = ELECTION_TIMEOUT
+		inter.reschedule(timeInterval)
+
+	} else if(state == CANDIDATE_STATE){
 		timeInterval = getRandomInt((10*700), (10*900))
 		console.log('Vote size: '+setVote.size)
+
 		if(setVote.size >= MAJORITY) {
-			console.log('Succes become leader')
-			sendHeartbreath()
-			timeInterval = 2000
 			state = LEADER_STATE
+			console.log('Success become leader')
+
+			sendHeartbeat()
+			timeInterval = HEARTBEAT_TIMEOUT
+			
 		} else {
 			console.log('Back to follower state')
-			setVote = new Set()
 			state = FOLLOWER_STATE
 		}
 
 		inter.reschedule(timeInterval) // reset schedule
 
-	} else if (state == FOLLOWER_STATE) {
-		state = 1
-		console.log("Send election")
-		broadcastElection()
-		timeInterval = 3000
-		inter.reschedule(timeInterval)
-
 	} else { // state == LEADER_STATE
-		console.log("Send hearthbreath")
 		updateServerData()
-		sendHeartbreath()	
+
+		console.log("Send hearthbeat to all nodes")
+		sendHeartbeat()	
 	}
 
 }, timeInterval)
+
+/* 
+ * Fungsi SendHeartbeat dipanggil oleh Leader
+ * untuk broadcasting data ke setiap node
+ */
+function sendHeartbeat() {
+	_.forEach(nodeData, function(obj,i){
+		if(i != selfData){
+			let url = 'http://'+obj.ip+':'+obj.port+'/' 
+			console.log('Send heartbeat to :'+url)
+			nodeData[selfData].serverData = serverData
+			nodeData[selfData].term = selfTerm
+			axios({
+				method: 'post',
+				url: url,
+				data: nodeData[selfData]
+			})
+			.then((data) => {
+				console.log('>> Node ' + i + ' is STILL ALIVE.')
+			})
+			.catch((err) => {
+				console.log('>> Node ' + i + ' is OFFLINE.')
+			})
+		}
+	})
+}
+
+/* 
+ * Fungsi UpdateServerData dipanggil oleh Leader
+ * Hasilnya dikirimkan melalui Heartbeat
+ */
+function updateServerData() { 
+	_.forEach(serverData, function(obj, i) {
+		let url = 'http://'+obj.ip+':'+obj.daemonPort+'/' 
+		console.log('Request workload to Daemon :'+url)
+		axios({
+			method: 'get',
+			url: url
+		})
+		.then((data) => {
+			var response = data.data
+			obj.workload = response.workload
+			obj.status = response.status
+		})
+		.catch((err) => {
+			console.log('>> Server ' + i + ' is OFFLINE.')
+			obj.workload = 1.0
+			obj.status = 'down'
+		})
+	})
+}
+
+/* 
+ * Fungsi BroadcastElection dipanggil oleh Candidate
+ * untuk memulai proses voting
+ */
+function broadcastElection() {
+	_.forEach(nodeData, function(obj,i){
+		if(i != selfData){
+			let url = 'http://'+obj.ip+':'+obj.port+'/'+selfTerm
+			console.log('Send to :'+url)
+			axios({
+				method: 'get',
+				url: url
+			})
+			.then((data) => {
+				if(data.data != '0') {
+					console.log('>> Node ' + i + ' VOTED for you.')
+					setVote.add(data.data)
+				} else {
+					console.log('>> Node ' + i + ' DOESN\'T like you </3.')
+				}
+			})
+			.catch((err) => {
+				console.log('>> Node ' + i + ' is OFFLINE.')
+			})
+		}
+	})
+}
 
 /***********************************************
  *             EXPRESS.JS ROUTING
@@ -120,26 +225,39 @@ var inter = reInterval(function () {
  ***********************************************/
 var urlencodedParser = bodyParser.json();
 
-app.post('/', urlencodedParser, function (req, res) { //respon Heartbreath
-	console.log("Follower")
+/* 
+ * Route untuk memproses "heartbeat" dari Leader Node
+ * 
+ */
+app.post('/', urlencodedParser, function (req, res) { 
+	console.log("I am a follower, and I have a leader.")
 	if(selfTerm <= req.body.term){
+		state = FOLLOWER_STATE
+
 		leader.ip = req.body.ip
 		leader.port = req.body.port
-		selfTerm = req.body.term
 		serverData = req.body.serverData
-		state = FOLLOWER_STATE
-		// console.log(serverData)
+		selfTerm = req.body.term
+
+		console.log('=========== CURRENT SERVER STATUS ============');
+		_.forEach(serverData, function(server, i) {
+			console.log('Server ' + i + ' workload = ' + (server.workload * 100.0).toFixed(2) + ' %'
+				+ '. Server is ' + ((server.status == 'up')? 'UP' : 'DOWN'))
+		});
+		console.log('==============================================');
+
 		inter.reschedule(timeInterval)
 	}
 	res.send('1')
 })
 
 app.get('/:term', function (req, res) { // respon election
-	let _term = parseInt(term)
-	if((state == FOLLOWER_STATE) && (_term >= selfTerm)) {
-		selfTerm++;	
+	let _term = parseInt(req.params.term)
+	if(_term > selfTerm) {
+		selfTerm = _term
 		inter.reschedule(timeInterval)
 		res.send(String(nodeData[selfData].port))
+
 	} else {
 		res.send('0')
 	}
@@ -166,87 +284,5 @@ app.get('/query/:n', function (req, res) {
 		console.log('Error: '+err)
 	})
 })
-
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min
-}
-
-/* 
- * Fungsi SendHeartbeat dipanggil oleh Leader
- * untuk broadcasting data ke setiap node
- */
-function sendHeartbreath() {
-	_.forEach(nodeData, function(obj,i){
-		if(i != selfData){
-			let url = 'http://'+obj.ip+':'+obj.port+'/' 
-			console.log('Send to :'+url)
-			nodeData[selfData].serverData = serverData
-			nodeData[selfData].term = selfTerm
-			axios({
-				method: 'post',
-				url: url,
-				data: nodeData[selfData]
-			})
-			.then((data) => {
-				console.log('Succes: '+data.data)
-				
-			})
-			.catch((err) => {
-				console.log('Error: '+err)
-			})
-		}
-	})
-}
-
-/* 
- * Fungsi UpdateServerData dipanggil oleh Leader
- * Hasilnya dikirimkan melalui Heartbeat
- */
-function updateServerData() { 
-	_.forEach(serverData, function(obj, i) {
-		let url = 'http://'+obj.ip+':'+obj.daemonPort+'/' 
-		console.log('Request workload to Daemon :'+url)
-		axios({
-			method: 'get',
-			url: url
-		})
-		.then((data) => {
-			var response = data.data
-			obj.workload = response.workload
-			obj.status = response.status
-		})
-		.catch((err) => {
-			console.log('Error: '+err)
-			obj.workload = 1.0
-			obj.status = 'down'
-		})
-	})
-}
-
-/* 
- * Fungsi BroadcastElection dipanggil oleh Candidate
- * untuk memulai proses voting
- */
-function broadcastElection() {
-	_.forEach(nodeData, function(obj,i){
-		if(i != selfData){
-			let url = 'http://'+obj.ip+':'+obj.port+'/'+selfTerm
-			console.log('Send to :'+url)
-			axios({
-				method: 'get',
-				url: url
-			})
-			.then((data) => {
-				console.log('Succes: '+data.data)
-				if(data != '0') {
-					setVote.add(data.data)
-				}
-			})
-			.catch((err) => {
-				console.log('Error: '+err)
-			})
-		}
-	})
-}
  
 app.listen(nodeData[selfData].port)
